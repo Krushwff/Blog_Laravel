5db6a45c5f79a3447738179e7ab48fd5fa6a9547
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-02 01:30:44 +0300

    Создана страница категории

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
     Route::group(['namespace' => 'Main'], function () {
         Route::get('/', 'IndexController')->name('index');
     });
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+        Route::get('/', 'IndexController')->name('categories.index');
 });
+});
+
 
 Auth::routes();
 
