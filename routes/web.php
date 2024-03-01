c0530424d87d377e01f80072d6ae1a36b3eab0e5
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-01 19:25:07 +0300

    Поставил фронт админки, установил Admin Lte

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 Route::group(['namespace' => 'App\Http\Controllers\Main'], function () {
     Route::get('/', 'IndexController')->name('index');
 });
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController')->name('index');
+    });
+});
 
 Auth::routes();
 
