86a9a3379c8e047a48bbd12f262d11358ca711c7
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-05 00:11:12 +0300

    исправление бага в бд

--- database/migrations/2024_03_04_210937_add_column_tags_title_to_tags_table.php
+++ database/migrations/2024_03_04_210937_add_column_tags_title_to_tags_table.php
@@ -22,13 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('tags', function (Blueprint $table) {
-
-            $table->dropColumn('post_id');
-            $table->dropColumn('tag_id');
-            //IDX
-            $table->dropColumn('post_id', 'post_tag_post_idx');
-            $table->dropColumn('tag_id', 'post_tag_tag_idx');
-
+            $table->dropColumn('title');
         });
     }
 };
