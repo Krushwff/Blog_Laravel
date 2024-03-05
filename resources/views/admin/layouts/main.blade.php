630a94875d5fb07a565b71b52e6c9dd31c85f15e
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-06 02:19:20 +0300

    Добавляем атрибуты для загрузки изображений

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -69,6 +69,7 @@
 <!-- daterangepicker -->
 <script src="{{asset('plugins/moment/moment.min.js')}}"></script>
 <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+<script src="{{asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
 <!-- overlayScrollbars -->
 <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
 <!-- AdminLTE App -->
@@ -86,6 +87,10 @@
                 ['height', ['height']]
             ]
         });
+    });
+
+        $(function () {
+        bsCustomFileInput.init();
     });
 </script>
 </body>
