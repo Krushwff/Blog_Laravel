e6bcad1a822c65c43265db3c9cd9f61a851f38f4
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-05 21:17:52 +0300

    основа cruda для постов

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -14,6 +14,14 @@
                 </a>
             </li>
             <li class="nav-item">
+                <a href="{{ route('admin.post.index') }}" class="nav-link">
+                    <i class="nav-icon far fa-clipboard"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
                 <a href="{{ route('admin.tag.index') }}" class="nav-link">
                     <i class="nav-icon fas fa-tags"></i>
                     <p>
