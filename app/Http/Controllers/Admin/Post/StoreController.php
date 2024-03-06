4c334ad45e54c4e83fe5ceebea9ae108558d798d
Krushwff <isip_g.p.krayushkin@mpt.ru>
2024-03-07 02:09:31 +0300

    Реализуем апдейт у постов, форма редактирования, отображение изображений

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -19,8 +19,9 @@ class StoreController extends Controller
 
 
 
-        $data['preview_image'] = Storage::put('/images', $data['preview_image']);
-        $data['main_image'] = Storage::put('/images', $data['main_image']);
+        $data['preview_image'] = Storage::disk('public')->put('/images', $data['preview_image']);
+        $data['main_image'] = Storage::disk('public')->put('/images', $data['main_image']);
+
         $post = Post::firstOrCreate($data);
         $post->tags()->attach($tagIds);
 
